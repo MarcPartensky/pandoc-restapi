c6f0da822a75dde1f69f4c395a6d463c9425f68f
marc <marc.partensky@gmail.com>
2020-12-03 02:46:59 +0100

    index page

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from flask import request, send_file
+from flask import request, send_file, render_template
 import pypandoc
 import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/echo/<text>')
 def echo(text:str):
     return text
 
-@app.route('/file/<filename>', methods=['GET', 'POST'])
+@app.route('/store/<filename>', methods=['GET', 'POST'])
 def file(filename:str):
     if request.method == 'POST':
         f = request.files['file']
@@ -18,6 +22,15 @@ def file(filename:str):
     elif request.method == 'GET':
         ext = filename.split('.')[-1]
         return send_file(f'files/{filename}', mimetype=f'plain/{ext}')
+
+@app.route('/pandoc-form', methods=['POST'])
+def pandoc_form():
+    output = request.form['format']
+    f = request.files['file']
+    f.save(f'files/{f.filename}')
+    response = pypandoc.convert_file(f'files/{f.filename}', output)
+    os.remove(f'files/{f.filename}')
+    return response
 
 @app.route('/pandoc/<output>', methods=['POST'])
 def pandoc(output:str):
