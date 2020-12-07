e9c94c8716af3733cac69e369ae7e014fb08435e
marc <marc.partensky@gmail.com>
2020-12-07 20:39:12 +0100

    better tab spacing

--- server.py
+++ server.py
@@ -2,8 +2,15 @@ from flask import Flask
 from flask import request, send_file, render_template
 import pypandoc
 import os
+import glob
 
 app = Flask(__name__)
+
+def clean():
+    """Clean the files folder."""
+    files = glob.glob('files/*')
+    for f in files:
+        os.remove(f)
 
 @app.route('/')
 def index():
@@ -28,16 +35,26 @@ def pandoc_form():
     output = request.form['format']
     f = request.files['file']
     f.save(f'files/{f.filename}')
-    response = pypandoc.convert_file(f'files/{f.filename}', output)
-    os.remove(f'files/{f.filename}')
+    # response = pypandoc.convert_file(f'files/{f.filename}', output)
+    # os.remove(f'files/{f.filename}')
+    cmd = f"pandoc files/{f.filename} -o files/{f.filename}.{output}"
+    print(cmd)
+    os.system(cmd)
+    with open(f'files/{output}', 'rb') as file:
+        response = file.read()
+    clean()
     return response
 
 @app.route('/pandoc/<output>', methods=['POST'])
 def pandoc(output:str):
     f = request.files['file']
     f.save(f'files/{f.filename}')
-    response = pypandoc.convert_file(f'files/{f.filename}', output)
-    os.remove(f'files/{f.filename}')
+    cmd = f"pandoc files/{f.filename} -o files/{f.filename}.{output}"
+    print(cmd)
+    os.system(cmd)
+    with open(f'files/{output}', 'rb') as file:
+        response = file.read()
+    clean()
     return response
 
 if __name__ == '__main__':
